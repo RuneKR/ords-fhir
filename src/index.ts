0fe56d3933d45d2d5498e8f0c0076c661e5f3a55
Rune Rasmussen <rune@medsolve.dk>
2016-05-09 15:21:48 +0200

    Exporting common

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 /// <reference path='../typings/main.d.ts' />
-import * as express from 'express';
+import * as express                 from 'express';
+import {DependencyInjector}         from './lib/DependencyInjector';
+import {HookManager}                from './lib/HookManager';
+import {DBManager}                  from './lib/DBManager';
 
-export {Server}     from './lib/server';
+export {Server}                     from './lib/server';
+export {DI}                         from './lib/DependencyInjector';
 
 export interface Common {
-    Router: any
+    DBManager: DBManager;
+    DependencyInjector: DependencyInjector;
+    HookManager: HookManager;
 }
 
-export const common: Common = {
+export const common: any = {
+    DBManager: DBManager,
+    DependencyInjector: DependencyInjector,
+    HookManager: HookManager,
     Router: express
 };
