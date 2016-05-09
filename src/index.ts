ad070b146427226d19fbf61c75728cf93ef2b3de
Rune Rasmussen <rune@medsolve.dk>
2016-05-09 16:12:24 +0200

    EOD

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
 /// <reference path='../typings/main.d.ts' />
-import * as express                 from 'express';
-import {DependencyInjector}         from './lib/DependencyInjector';
-import {HookManager}                from './lib/HookManager';
-import {DBManager}                  from './lib/DBManager';
 
 export {Server}                     from './lib/server';
-export {DI}                         from './lib/DependencyInjector';
-
-export interface Common {
-    DBManager: DBManager;
-    DependencyInjector: DependencyInjector;
-    HookManager: HookManager;
-}
-
-export const common: any = {
-    DBManager: DBManager,
-    DependencyInjector: DependencyInjector,
-    HookManager: HookManager,
-    Router: express
-};
+export {DI, DependencyInjector}     from './lib/DependencyInjector';
+export {HookManager}                from './lib/HookManager';
+export {DBManager}                  from './lib/DBManager';
+export {Requestparser}              from './lib/Requestparser';
