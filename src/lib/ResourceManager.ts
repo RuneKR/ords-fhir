6bcbd4a5d25e5d07fa016389149bac8c557b2ca3
Rune Rasmussen <rune@medsolve.dk>
2016-05-15 11:22:21 +0200

    Introduced ConformanceManager

--- src/lib/ResourceManager.ts
+++ src/lib/ResourceManager.ts
@@ -2,9 +2,6 @@ import * as models                  from '../resources/ModelList';
 import * as valueSets               from '../resources/valueSetList';
 import * as dataTypes               from '../resources/dataTypeList';
 
-import {IConformance, Conformance}  from '../resources/Conformance';
-import {Enforce}                    from 'ts-objectschema';
-
 /**
  * Manage every resource in the application
  */
@@ -24,17 +21,4 @@ export class ResourceManager {
      * @type {{[key: string]: any}}
      */
     public dataTypes: { [key: string]: any } = dataTypes;
-    /**
-     * Instanceiated conformance resource
-     */
-    public conformance: Conformance;
-    /**
-     * Build the conformance
-     * @param   {IConformance}  conformance     conformance to be builded
-     * @returns {Void}
-     */
-    public buildConformance(conformance: IConformance): void {
-
-        this.conformance = new Conformance(conformance, Enforce.required);
-    }
 }
