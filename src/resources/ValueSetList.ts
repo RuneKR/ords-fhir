f031b97c846da065c0d61dbb1c7c5faa14a84414
Rune Rasmussen <rune@medsolve.dk>
2016-05-16 08:23:17 +0200

    Corrected valueSet list

--- src/resources/ValueSetList.ts
+++ src/resources/ValueSetList.ts
@@ -1,6 +1,6 @@
 // list all valuesets that are activliy used
 
-export {addressUse}                from '../resources/valueSets/AddressUse';
-export {conformanceResourceStatus} from '../resources/valueSets/ConformanceResourceStatus';
-export {identifierUse}             from '../resources/valueSets/IdentifierUse';
-export {nameUse}                   from '../resources/valueSets/NameUse';
+export {AddressUse}                from '../resources/valueSets/AddressUse';
+export {ConformanceResourceStatus} from '../resources/valueSets/ConformanceResourceStatus';
+export {IdentifierUse}             from '../resources/valueSets/IdentifierUse';
+export {NameUse}                   from '../resources/valueSets/NameUse';
