e8b1a67bd04ef585fbdbabc5ecea2041705fb90f
Rune Rasmussen <rune@medsolve.dk>
2016-05-18 14:57:21 +0200

    Started on the boundle now were here

--- src/resources/internal/Boundle.ts
+++ src/resources/internal/Boundle.ts
@@ -1,4 +1,66 @@
+import * as tso        from 'ts-objectschema';
+import {boundleType}   from '../valueSets/BoundleType';
 
+export interface IBoundle {
+    type: string;
+    total?: number;
+    link?: Array<{
+        relation: string
+        url: string
+    }>;
+}
+
+@tso.decorators.validate
+export class Link {
+    public relation: tso.ElementDefinition = {
+        required: true,
+        type: tso.datatypes.String
+    };
+    public url: tso.ElementDefinition = {
+        required: true,
+        type: tso.datatypes.Uri
+    };
+}
+
+@tso.decorators.validate
+export class Entry {
+    public link: tso.ElementDefinition = {
+        required: false,
+        type: Link
+    };
+    public fulluri: tso.ElementDefinition = {
+        required: true,
+        type: tso.datatypes.Uri
+    };
+    public resource: tso.ElementDefinition = {
+        definition: 'The contained resource',
+        required: true,
+        type: tso.datatypes.Any
+    };
+}
+
+@tso.decorators.autoValidate(tso.Enforce.required)
 export class Boundle {
-    
+    public type: tso.ElementDefinition = {
+        binding: new tso.Binding(tso.BindingStrength.required, 'Boundle type', boundleType),
+        required: true,
+        type: tso.datatypes.String
+    };
+    public total: tso.ElementDefinition = {
+        required: false,
+        type: tso.datatypes.Number
+    };
+    public link: tso.ElementDefinition = {
+        required: false,
+        type: Link
+    };
+    public entry: tso.ElementDefinition = {
+        required: false,
+        type: Entry
+    };
+    constructor(data: IBoundle) {
+
+        // do nothing
+    }
+
 }
---
+++ src/resources/valueSets/BoundleType.ts
@@ -0,0 +1,32 @@
+import {Valueset} from 'ts-objectschema';
+
+export class BoundleType {
+    public codeSystem: Object = {
+        concept: [
+            {
+                abstract: true,
+                code: 'home',
+                display: 'string'
+            },
+            {
+                abstract: true,
+                code: 'work',
+                display: 'string'
+            },
+            {
+                abstract: true,
+                code: 'temp',
+                display: 'string'
+            },
+            {
+                abstract: true,
+                code: 'old',
+                display: 'string'
+            },
+        ],
+        system: process.env.domain + '/ValueSet/AddressUse',
+        version: '1.0.0'
+    };
+}
+
+export const boundleType: BoundleType = new Valueset(new BoundleType());
