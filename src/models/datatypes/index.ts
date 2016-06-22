486dfd6dff2172500e74be2f45164f5db7e296c4
Rune Rasmussen <rune@medsolve.dk>
2016-06-22 15:12:02 +0200

    Rename of files to correct structure

--- src/models/datatypes/index.ts
+++ src/models/datatypes/index.ts
@@ -1,1 +1,8 @@
-// list FHIR data types
+export {Address}                from './address';
+export {CodeableConcept}        from './codeable-concept';
+export {ContactPoint}           from './contact-point';
+export {HumanName}              from './human-name';
+export {Identifier}             from './identifier';
+export {Meta}                   from './meta';
+export {Period}                 from './period';
+export {Reference}              from './reference';
