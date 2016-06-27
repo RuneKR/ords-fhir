db5f8a9d23579c148c9412c2e7a343b3c4654097
Rune Rasmussen <rune@medsolve.dk>
2016-06-27 14:50:17 +0200

    Added search descriptor

--- src/components/conformance/models/resource.ts
+++ src/components/conformance/models/resource.ts
@@ -5,11 +5,28 @@ export interface RuleEntry {
     sourcekey: string;
 }
 
+// see https://www.hl7.org/fhir/searchparameter.html
+
+export interface SearchParameter {
+    /**
+     * FHIR search parameter type
+     */
+    type: string;
+    /**
+     * The code used in query
+     */
+    code: string;
+    /**
+     * The meaning of it
+     */
+    description: string;
+}
+
 export class Resource {
     /**
      * Parameters that can be queried / pre indexed variables
      */
-    public queryables: any = {};
+    public queryables: Array<SearchParameter>;
     /**
      * Schema for this resource
      */
