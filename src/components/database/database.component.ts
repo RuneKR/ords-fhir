88c36928431438b0df3dbb7e9cb3493a35a3a1da
Rune Rasmussen <rune@medsolve.dk>
2016-07-06 15:52:56 +0200

    Matched database with hookable

--- src/components/database/database.component.ts
+++ src/components/database/database.component.ts
@@ -1,298 +1,48 @@
-import {Promise}                            from 'es6-promise';
-import {Request, Response, RequestHandler}  from '../routing/routing.models';
-import {DatabaseMiddleware}                 from './database.middleware';
+import {RoutingModels}                      from '../routing';
+import {HookableComponent, HookableModels}  from '../hookable';
 import {DependencyInjectorComponent}        from '../dependency-injector';
 
 /**
- * Handles connection to a database by doing all middlewares
+ * Connect to a database and perform operation in that
  */
-@DependencyInjectorComponent.createWith(DatabaseMiddleware)
+@DependencyInjectorComponent.createWith(HookableComponent)
 export class DatabaseComponent {
     /**
-     * Ref to database middleware
+     * Create something in the database
      */
-    private dbm: DatabaseMiddleware;
+    public create: HookableModels.All<RoutingModels.Request>;
+    /**
+     * Read something from the database
+     */
+    public read: HookableModels.All<RoutingModels.Request>;
+    /**
+     * Update something in the database
+     */
+    public update: HookableModels.All<RoutingModels.Request>;
+    /**
+     * Create something in the database
+     */
+    public delete: HookableModels.All<RoutingModels.Request>;
+    /**
+     * Read history of something in the database
+     */
+    public history: HookableModels.All<RoutingModels.Request>;
+    /**
+     * Patch something in the database
+     */
+    public patch: HookableModels.All<RoutingModels.Request>;
     /**
      * Create new instance of DBManager and bind middleware to it
      */
-    constructor(dbm: DatabaseMiddleware) {
+    constructor(hc: HookableComponent) {
+
+        // init hookable
+        this.create = hc.threeLayer<RoutingModels.Request>();
+        this.read = hc.threeLayer<RoutingModels.Request>();
+        this.update = hc.threeLayer<RoutingModels.Request>();
+        this.delete = hc.threeLayer<RoutingModels.Request>();
+        this.history = hc.threeLayer<RoutingModels.Request>();
+        this.patch = hc.threeLayer<RoutingModels.Request>();
 
-        // bind middleware
-        this.dbm = dbm;
-    }
-    /**
-     * Create something in the database
-     * @param   {Request}     req     requrest from the client
-     * @param   {Response}    res     response being send to the client
-     * @returns {Promise}     send back response when all middleware has been run
-     */
-    public create(req: Request, res: Response): Promise<any> {
-
-        // return promise about resolution of task
-        return new Promise((resolve: Function, reject: Function) => {
-
-            // create stack that are being run
-            let stack: Array<RequestHandler> = [];
-
-            Array.prototype.push.apply(stack, this.dbm.filters.input.create);
-            Array.prototype.push.apply(stack, this.dbm.actors.create);
-            Array.prototype.push.apply(stack, this.dbm.filters.output.create);
-
-            // run stack
-            let next: any = function (): void {
-
-                // check if any functions are left to run
-                if (stack.length === 0) {
-                    resolve();
-                }
-
-                // next function to run
-                let func: RequestHandler = stack.shift();
-
-                // try to run through the whole stack
-                try {
-                    func(req, res, next);
-
-                    // if error is made then operation outcome is updated there
-                } catch (err) {
-
-                    // perhaps catch runtime errors that did not set the correct operationoutcome?
-
-                    reject();
-                }
-            };
-
-            // start next
-            next();
-        });
-    }
-    /**
-     * Read something from the database
-     * @param   {Request}     req     requrest from the client
-     * @param   {Response}    res     response being send to the client
-     * @returns {Promise}     send back response when all middleware has been run
-     */
-    public read(req: Request, res: Response): Promise<any> {
-
-        // return promise about resolution of task
-        return new Promise((resolve: Function, reject: Function) => {
-
-            // create stack that are being run
-            let stack: Array<RequestHandler> = [];
-
-            Array.prototype.push.apply(stack, this.dbm.filters.input.read);
-            Array.prototype.push.apply(stack, this.dbm.actors.read);
-            Array.prototype.push.apply(stack, this.dbm.filters.output.read);
-
-            // run stack
-            let next: any = function (): void {
-
-                // check if any functions are left to run
-                if (stack.length === 0) {
-                    resolve();
-                }
-
-                // next function to run
-                let func: RequestHandler = stack.shift();
-
-                // try to run through the whole stack
-                try {
-                    func(req, res, next);
-
-                    // if error is made then operation outcome is updated there
-                } catch (err) {
-
-                    // perhaps catch runtime errors that did not set the correct operationoutcome?
-
-                    reject();
-                }
-            };
-
-            // start next
-            next();
-        });
-    }
-    /**
-     * Update something in the database
-     * @param   {Request}     req     requrest from the client
-     * @param   {Response}    res     response being send to the client
-     * @returns {Promise}     send back response when all middleware has been run
-     */
-    public update(req: Request, res: Response): Promise<any> {
-
-        // return promise about resolution of task
-        return new Promise((resolve: Function, reject: Function) => {
-
-            // create stack that are being run
-            let stack: Array<RequestHandler> = [];
-
-            Array.prototype.push.apply(stack, this.dbm.filters.input.update);
-            Array.prototype.push.apply(stack, this.dbm.actors.update);
-            Array.prototype.push.apply(stack, this.dbm.filters.output.update);
-
-            // run stack
-            let next: any = function (): void {
-
-                // check if any functions are left to run
-                if (stack.length === 0) {
-                    resolve();
-                }
-
-                // next function to run
-                let func: RequestHandler = stack.shift();
-
-                // try to run through the whole stack
-                try {
-                    func(req, res, next);
-
-                    // if error is made then operation outcome is updated there
-                } catch (err) {
-
-                    // perhaps catch runtime errors that did not set the correct operationoutcome?
-
-                    reject();
-                }
-            };
-
-            // start next
-            next();
-        });
-    }
-    /**
-     * Create something in the database
-     * @param   {Request}     req     requrest from the client
-     * @param   {Response}    res     response being send to the client
-     * @returns {Promise}     send back response when all middleware has been run
-     */
-    public delete(req: Request, res: Response): Promise<any> {
-
-        // return promise about resolution of task
-        return new Promise((resolve: Function, reject: Function) => {
-
-            // create stack that are being run
-            let stack: Array<RequestHandler> = [];
-
-            Array.prototype.push.apply(stack, this.dbm.filters.input.delete);
-            Array.prototype.push.apply(stack, this.dbm.actors.delete);
-            Array.prototype.push.apply(stack, this.dbm.filters.output.delete);
-
-            // run stack
-            let next: any = function (): void {
-
-                // check if any functions are left to run
-                if (stack.length === 0) {
-                    resolve();
-                }
-
-                // next function to run
-                let func: RequestHandler = stack.shift();
-
-                // try to run through the whole stack
-                try {
-                    func(req, res, next);
-
-                    // if error is made then operation outcome is updated there
-                } catch (err) {
-
-                    // perhaps catch runtime errors that did not set the correct operationoutcome?
-
-                    reject();
-                }
-            };
-            // start next
-            next();
-        });
-    }
-    /**
-     * Read history of something in the database
-     * @param   {Request}     req     requrest from the client
-     * @param   {Response}    res     response being send to the client
-     * @returns {Promise}     send back response when all middleware has been run
-     */
-    public history(req: Request, res: Response): Promise<any> {
-
-        // return promise about resolution of task
-        return new Promise((resolve: Function, reject: Function) => {
-
-            // create stack that are being run
-            let stack: Array<RequestHandler> = [];
-
-            Array.prototype.push.apply(stack, this.dbm.filters.input.history);
-            Array.prototype.push.apply(stack, this.dbm.actors.history);
-            Array.prototype.push.apply(stack, this.dbm.filters.output.history);
-
-            // run stack
-            let next: any = function (): void {
-
-                // check if any functions are left to run
-                if (stack.length === 0) {
-                    resolve();
-                }
-
-                // next function to run
-                let func: RequestHandler = stack.shift();
-
-                // try to run through the whole stack
-                try {
-                    func(req, res, next);
-
-                    // if error is made then operation outcome is updated there
-                } catch (err) {
-
-                    // perhaps catch runtime errors that did not set the correct operationoutcome?
-
-                    reject();
-                }
-            };
-
-            // start next
-            next();
-        });
-    }
-    /**
-     * Patch something in the database
-     * @param   {Request}     req     requrest from the client
-     * @param   {Response}    res     response being send to the client
-     * @returns {Promise}     send back response when all middleware has been run
-     */
-    public patch(req: Request, res: Response): Promise<any> {
-
-        // return promise about resolution of task
-        return new Promise((resolve: Function, reject: Function) => {
-
-            // create stack that are being run
-            let stack: Array<RequestHandler> = [];
-
-            Array.prototype.push.apply(stack, this.dbm.filters.input.patch);
-            Array.prototype.push.apply(stack, this.dbm.actors.patch);
-            Array.prototype.push.apply(stack, this.dbm.filters.output.patch);
-
-            // run stack
-            let next: any = function (): void {
-
-                // check if any functions are left to run
-                if (stack.length === 0) {
-                    resolve();
-                }
-
-                // next function to run
-                let func: RequestHandler = stack.shift();
-
-                // try to run through the whole stack
-                try {
-                    func(req, res, next);
-
-                    // if error is made then operation outcome is updated there
-                } catch (err) {
-
-                    // perhaps catch runtime errors that did not set the correct operationoutcome?
-
-                    reject();
-                }
-            };
-
-            // start next
-            next();
-        });
     }
 }
