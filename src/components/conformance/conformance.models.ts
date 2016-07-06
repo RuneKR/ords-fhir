443f839b5022f91a69aeda16fa22a04149cbb402
Rune Rasmussen <rune@medsolve.dk>
2016-07-06 18:05:08 +0200

    Moving conform data types and valuesets to conformance

--- src/components/conformance/conformance.models.ts
+++ src/components/conformance/conformance.models.ts
@@ -1,1 +1,6 @@
 export {ConformResource}            from './models/conform-resource';
+import * as Datatypes       from './models/datatypes'; export {Datatypes}
+export {DomainResource}     from './models/general/domain-resource';
+export {ValueSet}           from './models/general/value-set';
+export {ElementDefinition}  from './models/general/element-definition';
+export {StructureDefinition}     from './models/general/structure-definition';
--- src/components/schema/schema.models.ts
+++ src/components/schema/schema.models.ts
@@ -3,8 +3,3 @@ export {PropertyDefinition} from './models/property-definition';
 export {Enforce}            from './models/enforce';
 export {Element}            from './models/element';
 export {Schema}             from './models/schema';
-import * as Datatypes       from './models/datatypes'; export {Datatypes}
-export {DomainResource}     from './models/general/domain-resource';
-export {ValueSet}           from './models/general/value-set';
-export {ElementDefinition}  from './models/general/element-definition';
-export {StructureDefinition}     from './models/general/structure-definition';
