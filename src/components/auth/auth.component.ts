5d87b4affeea32ad7199659df6d927ba09e00849
Rune Rasmussen <rune@medsolve.dk>
2016-07-06 17:11:34 +0200

    Filling commands in auth

--- src/components/auth/auth.component.ts
+++ src/components/auth/auth.component.ts
@@ -1,8 +1,39 @@
-import {Request, Response, NextFunction}      from    'express';    
+import {RoutingModels}                      from '../routing';
+import {HookableComponent, HookableModels}  from '../hookable';
+import {DependencyInjectorComponent}        from '../dependency-injector';
+import {User}                               from './models/user';
 
-// ideas for what a user should contain?
+/**
+ * Connect to a database and perform operation in that
+ */
+@DependencyInjectorComponent.createWith(HookableComponent)
 export class AuthComponent {
-    /*public authenticate(req: ) {
+    /**
+     * Get the user performing a request
+     */
+    public getUser: HookableModels.All<RoutingModels.Request, User>;
+    /**
+     * Create a new user based upon the information in the request
+     */
+    public createUser: HookableModels.All<RoutingModels.Request, User>;
+    /**
+     * Update information about the user
+     */
+    public updateUser: HookableModels.All<RoutingModels.Request, User>;
+    /**
+     * Delete the user performing the request
+     */
+    public deleteUser: HookableModels.All<RoutingModels.Request, User>;
+    /**
+     * Create new instance of DBManager and bind middleware to it
+     */
+    constructor(hc: HookableComponent) {
 
-    }*/
+        // init hookable
+        this.getUser     = hc.threeLayer<RoutingModels.Request, User>();
+        this.createUser  = hc.threeLayer<RoutingModels.Request, User>();
+        this.updateUser  = hc.threeLayer<RoutingModels.Request, User>();
+        this.deleteUser  = hc.threeLayer<RoutingModels.Request, User>();
+
+    }
 }
