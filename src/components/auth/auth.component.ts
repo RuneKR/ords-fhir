9dd2619ffd6628164e8b32752f63f85457b08645
Rune Rasmussen <rune@medsolve.dk>
2016-07-08 15:47:19 +0200

    Set auth to only returnable

--- src/components/auth/auth.component.ts
+++ src/components/auth/auth.component.ts
@@ -11,29 +11,29 @@ export class AuthComponent {
     /**
      * Get the user performing a request
      */
-    public getUser: HookableModels.ReturnableAll<Request, User>;
+    public getUser: HookableModels.Returnable<Request, User>;
     /**
      * Create a new user based upon the information in the request
      */
-    public createUser: HookableModels.ReturnableAll<Request, User>;
+    public createUser: HookableModels.Returnable<Request, User>;
     /**
      * Update information about the user
      */
-    public updateUser: HookableModels.ReturnableAll<Request, User>;
+    public updateUser: HookableModels.Returnable<Request, User>;
     /**
      * Delete the user performing the request
      */
-    public deleteUser: HookableModels.ReturnableAll<Request, User>;
+    public deleteUser: HookableModels.Returnable<Request, User>;
     /**
      * Create new instance of DBManager and bind middleware to it
      */
     constructor(hc: HookableComponent) {
 
         // init hookable
-        this.getUser     = hc.returnableAll();
-        this.createUser  = hc.returnableAll();
-        this.updateUser  = hc.returnableAll();
-        this.deleteUser  = hc.returnableAll();
+        this.getUser     = hc.returnable();
+        this.createUser  = hc.returnable();
+        this.updateUser  = hc.returnable();
+        this.deleteUser  = hc.returnable();
 
     }
 }
