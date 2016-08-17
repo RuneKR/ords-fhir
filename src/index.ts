9ceb9122a4301349cb7b1cef2298ed6c25a1d490
Rune <rune@Runes-iMac.local>
2016-08-17 10:33:54 +0200

    Considering config files for component

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 export *  from './lib/database';
 export *  from './lib/conformance';
 
-export *  from './client-connection/routing';
+export *  from './lib/client-connection/routing';
 
 // application
 export {Application}          from './app/application.component';
@@ -10,4 +10,3 @@ export {Application}          from './app/application.component';
 // shared
 import * as Models            from './shared/models'; export {Models};
 import * as Services          from './shared/services/constants'; export {Services}
-
