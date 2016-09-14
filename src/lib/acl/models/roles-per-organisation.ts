edf334c2ad7d4b9c97bdb1c210c86751af0f3962
MedSolve <rune@medsolve.dk>
2016-09-14 17:04:30 +1200

    Clean ACL and clean up circular dependency in db

--- src/lib/acl/models/roles-per-organisation.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Organisation sorted roles a user can have
- */
-export interface RolesPerOrganisation {
-    [organisaitonId: string]: string;
-}
--- src/lib/database/models/resource.ts
+++ src/lib/database/models/resource.ts
@@ -1,11 +1,34 @@
 import {SchemaModels}           from 'simple-ts-schema';
 import {QueryBase}              from './query-base';
+import {RecordRemoval}          from './record-removal';
+import {DataManipulation}       from './data-manipulation';
 
 export class ACL {
     /**
-     * Limit read operations in the database
+    * Limit create operations on the resource
+    */
+    public create(query: DataManipulation): void {
+
+        // do something here like updating the content
+    }
+    /**
+     * Limit read operations on the resource
      */
-    public Read(query: QueryBase): void {
+    public read(query: QueryBase): void {
+
+        // do something here like updating the content
+    }
+    /**
+    * Limit update operations on the resource
+    */
+    public update(query: DataManipulation): void {
+
+        // do something here like updating the content
+    }
+    /**
+    * Limit delete operations on the resource
+    */
+    public delete(query: RecordRemoval): void {
 
         // do something here like updating the content
     }
@@ -28,7 +51,7 @@ export class Resource {
      * Create a new resource conform to ORDS
      */
     constructor(data: Resource) {
-        
+
         this.name = data.name;
         this.schema = data.schema;
         this.acl = data.acl;
